1fca25e1cab84c10d133879ec85cbc7d8e8e0856
inman <sf990668852@gmail.com>
2024-04-17 13:27:21 +0800

    update

--- CPP011_For.cpp
+++ CPP011_For.cpp
@@ -3,29 +3,33 @@
  * Created:   18.11.2019
  **/
 
- #include<iostream>
- using namespace std;
+#include <iostream>
+using namespace std;
 
- int main() {
+int main()
+{
     /*
         for(initialization;condition;inc/dec)
             instructions_to_repeat;
     */
 
-    for(int i=0;i<3;i++)
-        cout<<"Hello"<<endl;
+    for (int i = 0; i < 3; i++)
+        cout << "Hello" << endl;
 
     int array[4];
 
-    for(int i=0;i<4;i++)
-        array[i]=i; //Initialization
+    for (int i = 0; i < 4; i++)
+        array[i] = i; // Initialization
 
-    for(int i=0;i<4;i++)
-        cout<<array[i]<<endl; //Print array elements
+    for (int i = 0; i < 4; i++)
+        cout << array[i] << endl; // Print array elements
 
-    //Infinite Loop
+    for (int i = 0; i < sizeof(array) / sizeof(array[0]); i++)
+        cout << array[i] << endl;
+
+    // Infinite Loop
     /*
     for(;;)
         cout<<"Stop me if you can !"<<endl;
     */
- }
+}
