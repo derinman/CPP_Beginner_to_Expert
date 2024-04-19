e5fc8cb749f7c4057903994262aa0e829fecec6b
inman <inman.hsueh@techfi.io>
2024-04-20 00:05:33 +0800

    update

--- CPP022_ReferenceVariables_Functions.cpp
+++ CPP022_ReferenceVariables_Functions.cpp
@@ -3,26 +3,42 @@
  * Created:   16.01.2020
  **/
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void swap(int &, int &);
+void swap1(int &, int &);
+void swap2(int, int);
 
-int main() {
+int main()
+{
 
-    int a=10,b=20;
-    swap(a,b);
+    int a = 10, b = 20;
+    swap1(a, b);
 
-    cout<<"a : "<<a<<endl;
-    cout<<"b : "<<b<<endl;
+    cout << "a : " << a << endl;
+    cout << "b : " << b << endl;
+
+    int c = 30, d = 40;
+    swap2(c, d);
+
+    cout << "c : " << c << endl;
+    cout << "d : " << d << endl;
 
     return 0;
 }
 
-void swap(int &a, int &b) {
+void swap1(int &x, int &y)
+{
     int t;
-    t=a;
-    a=b;
-    b=t;
+    t = x;
+    x = y;
+    y = t;
+}
 
+void swap2(int x, int y)
+{
+    int t;
+    t = x;
+    x = y;
+    y = t;
 }
